94f07de4c39c839eb6aa099c34a08bcf157a00a1
AbdUlRahmanShawareb <bzsc1010@gmail.com>
2021-04-09 05:50:01 +0200

    Add username to sign up, edit user profile pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,6 +10,12 @@ class PagesController < ApplicationController
 
   # back-end code for pages/profile
   def profile
+    if User.find_by_username(params[:id])
+      @username = params[:id]
+    else
+      flash[:danger] = "User not found"
+      redirect_to root_path
+    end
   end
 
   # back-end code for pages/explore
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # Routes for pages
   get '/home', to: 'pages#home'
-  get '/profile', to: 'pages#profile'
+  get '/user/:id', to: 'pages#profile'
   get '/explore', to: 'pages#explore'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
